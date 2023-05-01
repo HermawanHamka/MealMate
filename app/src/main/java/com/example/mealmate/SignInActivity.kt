b7cfb951e59f09d475862f1bddd6fcca00b72ddd
Zaky <zakyadrian321@gmail.com>
2023-05-01 09:48:46 +0700

    Sign in PW

--- app/src/main/java/com/example/mealmate/SignInActivity.kt
+++ app/src/main/java/com/example/mealmate/SignInActivity.kt
@@ -1,11 +1,18 @@
 package com.example.mealmate
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.text.Editable
 import android.text.TextWatcher
+import android.text.method.HideReturnsTransformationMethod
+import android.text.method.PasswordTransformationMethod
+import android.widget.ImageButton
+import android.widget.ImageView
+import android.widget.TextView
+
 
 class SignInActivity : AppCompatActivity() {
     private lateinit var emailEditText: EditText
@@ -14,20 +21,46 @@ class SignInActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_in)
+        //Pindah ke Register
+        val btnRegis = findViewById<TextView>(R.id.btnRegis)
+
+        btnRegis.setOnClickListener {
+            val intent = Intent(this, RegisterActivity::class.java)
+            startActivity(intent)
+        }
 
         emailEditText = findViewById<EditText>(R.id.inputEmail)
         passwordEditText = findViewById<EditText>(R.id.inputSandi)
         loginButton = findViewById<Button>(R.id.button)
 
+        //Show hide pw
+        passwordEditText.transformationMethod = PasswordTransformationMethod()
+        val showHideButton = passwordEditText.compoundDrawables
+
+        showHideButton.setOnClickListener {
+            if (passwordEditText.transformationMethod == PasswordTransformationMethod.getInstance()) {
+                passwordEditText.transformationMethod = HideReturnsTransformationMethod.getInstance()
+                showHideButton.isSelected = true
+            } else {
+                passwordEditText.transformationMethod = PasswordTransformationMethod.getInstance()
+                showHideButton.isSelected = false
+            }
+        }
+
         // Add TextChangedListener to EditTexts
         emailEditText.addTextChangedListener(textWatcher)
         passwordEditText.addTextChangedListener(textWatcher)
 
-
-
         // Disable login button
         loginButton.isEnabled = false
-        // Set enabled state and reset background color
+        loginButton.setBackgroundResource(R.drawable.btn_disable)
+        loginButton.alpha = 0.5f
+
+        //Button masuk klik
+        loginButton.setOnClickListener{
+            val intent = Intent(this, HomeActivity::class.java)
+            startActivity(intent)
+        }
     }
 
     private val textWatcher = object : TextWatcher {
@@ -35,8 +68,17 @@ class SignInActivity : AppCompatActivity() {
             val email = emailEditText.text.toString()
             val password = passwordEditText.text.toString()
 
-            // Enable login button if email and password are not empty
-            loginButton.isEnabled = email.isNotEmpty() && password.isNotEmpty()
+            if (email.isNotEmpty() && password.isNotEmpty()) {
+                // Enable login button and set alpha to 1.0
+                loginButton.isEnabled = true
+                loginButton.setBackgroundResource(R.drawable.btn_large)
+                loginButton.alpha = 1.0f
+            } else {
+                // Disable login button and set alpha to 0.5
+                loginButton.isEnabled = false
+                loginButton.setBackgroundResource(R.drawable.btn_disable)
+                loginButton.alpha = 0.5f
+            }
         }
 
         override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
